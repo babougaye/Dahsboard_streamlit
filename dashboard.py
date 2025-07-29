383dc0ce602c1c73a2cbd9f23a3dc7956e43cdbe
KGinfo <129065290+babougaye@users.noreply.github.com>
2025-07-29 12:47:00 +0000

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,8 @@
+Projet Streamlit 2025 
+Dans ce mini projet vous apprendrez à mettre en place un Dashboard  
+interactif sur Streamlit.   
+Pour les données, il s’agit d’une vente de produits aux USA avec des  
+variables comme : date de commande, statut de la commande, le  
+prix, la remise (discount) , le pays, la région, l’identifiant du client etc   
+Il vous est demandé de mettre en place un Dashboard sur Streamlit  
+avec les éléments suivants :
